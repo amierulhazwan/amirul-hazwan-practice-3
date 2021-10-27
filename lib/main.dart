2f2712e3e6c42a112f007d248c0e35b6f5c48159
Amirul Hazwan <amirulhazwan1997@gmail.com>
2021-10-28 06:44:26 +0800

    Practice Update

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,18 @@ class MyApp extends StatelessWidget {
   final emailController = TextEditingController();
   final pswdController = TextEditingController();
 
+  final _value = 1;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
-        title: Text("AppBar"),
-        backgroundColor: Colors.greenAccent[400],
+        title: const Text("Hello Deriv"),
+        backgroundColor: Colors.amber[900],
         elevation: 50.0,
         leading: IconButton(
-          icon: Icon(Icons.menu),
+          icon: const Icon(Icons.menu),
           tooltip: 'Menu Icon',
           onPressed: () {},
         ), //IconButton
@@ -24,61 +26,98 @@ class MyApp extends StatelessWidget {
       body: ListView(children: <Widget>[
         Center(
           child: Container(
-            padding: EdgeInsets.only(top: 40),
-            child: Text(
-              "Hello World",
+            padding: const EdgeInsets.only(top: 35),
+            child: const Text(
+              "Welcome to BeSquare",
               style: TextStyle(fontSize: 24),
             ),
           ),
         ),
         Container(
+          height: 250,
           child: Image.network(
             'https://scontent.fkul8-1.fna.fbcdn.net/v/t1.6435-9/167989111_122052736569342_2932569327583921419_n.png?_nc_cat=107&ccb=1-5&_nc_sid=973b4a&_nc_ohc=NSuR_bm2vZAAX-1kYGy&_nc_ht=scontent.fkul8-1.fna&oh=46bdd3dd9e3d12e4e7e5063c5a78ed25&oe=619D1175',
-            width: 300,
           ),
         ),
         Container(
-          padding: EdgeInsets.all(10),
+          height: 60,
+          padding: const EdgeInsets.all(10),
           child: TextField(
             controller: nameController,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               border: OutlineInputBorder(),
               labelText: 'Name',
             ),
           ),
         ),
         Container(
-          padding: EdgeInsets.all(10),
+          height: 60,
+          padding: const EdgeInsets.all(10),
           child: TextField(
             controller: emailController,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               border: OutlineInputBorder(),
               labelText: 'Email',
             ),
           ),
         ),
         Container(
-          padding: EdgeInsets.all(10),
+          height: 60,
+          padding: const EdgeInsets.all(10),
           child: TextField(
+            obscureText: true,
             controller: pswdController,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               border: OutlineInputBorder(),
               labelText: 'Password',
             ),
           ),
         ),
-        Container(),
         Container(
-            height: 100,
-            width: 50,
-            padding: EdgeInsets.all(10),
+          // height: 60,
+          padding: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(10)),
+          child: DropdownButton(
+            value: _value,
+            items: const [
+              DropdownMenuItem(
+                child: Text("Male"),
+                value: 1,
+              ),
+              DropdownMenuItem(
+                child: Text("Female"),
+                value: 2,
+              ),
+              DropdownMenuItem(
+                child: Text("Other"),
+                value: 3,
+              ),
+              DropdownMenuItem(
+                child: Text("Rather not to say"),
+                value: 4,
+              ),
+            ],
+
+            // onChanged: (int? value) {
+            //   setState(() {
+            //     _value = value!;
+            //   });
+            // },
+          ),
+        ),
+        Container(
+            height: 50,
+            padding: const EdgeInsets.all(10),
             child: ElevatedButton(
               style: ElevatedButton.styleFrom(
                 primary: Colors.amber[900],
               ),
-              child: Text('Button'),
+              child: const Text('Sign Up'),
               onPressed: () {
-                // print(inputController.text);
+                print(nameController.text);
+                print(emailController.text);
+                print(pswdController.text);
               },
             )),
       ] //Text
